f8408503941b81ace5879cfacd9cfd5801ead49a
AssassinG <hangq_zju@hotmail.com>
2018-12-15 16:43:16 +0800

    测试从服务端到消费端的异常捕获： 1.在ServiceImpl的方法中显式抛出异常throws(不成功) 2.在ServiceImpl的方法中try catch异常后在catch中抛出(不成功) 3.在Service接口中申明throws-2(成功了，但必须要申明在最终的接口中，在父接口中申明无效果，所以不太可行) 4.将异常类和facade类放在同一个包下(不成功) 5.再试一下方法3

--- src/main/java/cn/AssassinG/ScsyERP/User/facade/service/LoginableService.java
+++ src/main/java/cn/AssassinG/ScsyERP/User/facade/service/LoginableService.java
@@ -3,8 +3,10 @@ package cn.AssassinG.ScsyERP.User.facade.service;
 import cn.AssassinG.ScsyERP.User.facade.entity.User;
 import cn.AssassinG.ScsyERP.common.core.service.BaseService;
 import cn.AssassinG.ScsyERP.common.entity.LoginableEntity;
+import cn.AssassinG.ScsyERP.common.exceptions.BizException;
+import cn.AssassinG.ScsyERP.common.exceptions.DaoException;
 
 public interface LoginableService<T extends LoginableEntity> extends BaseService<T> {
-    Long createWithUser(T entity, User user);
+    Long createWithUser(T entity, User user) throws DaoException, BizException;
     void deleteByUserId(Long userId);
 }
