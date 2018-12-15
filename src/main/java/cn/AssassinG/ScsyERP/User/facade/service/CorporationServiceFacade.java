9712f4cbd6aae1c979c2fc0c9c76217bd347fb07
AssassinG <hangq_zju@hotmail.com>
2018-12-15 16:28:27 +0800

    测试从服务端到消费端的异常捕获： 1.在ServiceImpl的方法中显式抛出异常throws(不成功) 2.在ServiceImpl的方法中try catch异常后在catch中抛出(不成功) 3.在Service接口中申明throws-2

--- src/main/java/cn/AssassinG/ScsyERP/User/facade/service/CorporationServiceFacade.java
+++ src/main/java/cn/AssassinG/ScsyERP/User/facade/service/CorporationServiceFacade.java
@@ -2,9 +2,11 @@ package cn.AssassinG.ScsyERP.User.facade.service;
 
 import cn.AssassinG.ScsyERP.User.facade.entity.Corporation;
 import cn.AssassinG.ScsyERP.User.facade.entity.User;
+import cn.AssassinG.ScsyERP.common.exceptions.BizException;
+import cn.AssassinG.ScsyERP.common.exceptions.DaoException;
 
 import java.util.Map;
 
 public interface CorporationServiceFacade extends LoginableService<Corporation> {
-    Long create(String token, User user, Map<String, Object> paramMap);
+    Long create(String token, User user, Map<String, Object> paramMap) throws DaoException, BizException;
 }
